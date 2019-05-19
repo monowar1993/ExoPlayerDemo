364baacc9082989763ba69a23df4e73012d4b144
Mostafa Monowar <monowar1993@gmail.com>
2019-05-20 03:06:35 +0600

    query external storage for videos

--- app/src/main/java/com/exoplayerdemo/android/ui/main/MainActivity.kt
+++ app/src/main/java/com/exoplayerdemo/android/ui/main/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.exoplayerdemo.android.ui.main
 
+import android.Manifest
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
@@ -10,11 +10,18 @@ import androidx.lifecycle.ViewModelProviders
 import com.exoplayerdemo.android.R
 import com.exoplayerdemo.android.core.activity.AppActivity
 import com.exoplayerdemo.android.databinding.ActivityMainBinding
-
+import com.google.android.material.snackbar.Snackbar
+import com.orhanobut.logger.Logger
 import kotlinx.android.synthetic.main.activity_main.*
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
 import javax.inject.Inject
 
 class MainActivity : AppActivity<MainViewModel, ActivityMainBinding>() {
+
+    companion object {
+        private const val REQUEST_CODE_STORAGE_PERMISSION = 100
+    }
 
     @Inject
     lateinit var factory: ViewModelProvider.Factory
@@ -31,6 +38,8 @@ class MainActivity : AppActivity<MainViewModel, ActivityMainBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setSupportActionBar(toolbar)
+
+        getAllVideos()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -51,6 +60,38 @@ class MainActivity : AppActivity<MainViewModel, ActivityMainBinding>() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    @AfterPermissionGranted(REQUEST_CODE_STORAGE_PERMISSION)
+    private fun getAllVideos() {
+        val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (EasyPermissions.hasPermissions(getContext(), *permissions)) {
+            val videoItemHashSet = HashSet<String>()
+            val uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+            val projection = arrayOf(MediaStore.Video.VideoColumns.DATA, MediaStore.Video.Media.DISPLAY_NAME)
+            //val orderBy = MediaStore.Video.Media.DATE_TAKEN
+            val cursor = getContext().contentResolver.query(uri, projection, null, null, /*"$orderBy DESC"*/null)
+            try {
+                cursor!!.moveToFirst()
+                do {
+                    videoItemHashSet.add(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA)))
+                } while (cursor.moveToNext())
+
+                cursor.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            //return ArrayList(videoItemHashSet)
+            Logger.d(videoItemHashSet.toString())
+        } else {
+            EasyPermissions.requestPermissions(getActivity(), getString(R.string.storage_rational), REQUEST_CODE_STORAGE_PERMISSION, *permissions)
         }
     }
 }
